5d54b450620ac1c6162fc8822fa4aa80a093cd10
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-02-23 00:50:41 +0600

    Done

---
+++ Prime number/1 primeBruteforces 2.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if(n<2) return false;
+    for(long long i=2; i*i<=n; i++)
+    {
+        if(n%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+   int n;
+   cin >>n;
+
+   if(isPrime(n))
+   {
+      cout<< "Prime" <<endl;
+   }
+   else 
+  {
+      cout<< "Not Prime"<<endl;
+  }
+
+    return 0;
+}
